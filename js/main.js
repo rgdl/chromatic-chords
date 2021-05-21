b687f5b3421191fedd80f4d2f04e6a7348108d7d
rgdl <robert_delisle44@hotmail.com>
2021-05-21 20:42:58 +1000

    refactor to OO design

--- js/main.js
+++ js/main.js
@@ -1,71 +1,73 @@
-// TODO: refactor to OOP style once basic functionality is there
 // TODO: because of CORS issues, need to supply over a local server if I want importable modules. This may become necessary if the file grows too much
 // TODO: position with flex box or grid instead?
 const CONTAINER_SIZE = 60;
 const NODE_SIZE = CONTAINER_SIZE / 6;
 
-function applyStyleToNode(node, style) {
-    Object.keys(style).map(k => node.style[k] = style[k]);
+class VisualElement {
+    constructor(node, style = {}) {
+        this.node = node;
+        this.applyStyle(style);
+    }
+
+    applyStyle(style) {
+        Object.keys(style).map(k => this.node.style[k] = style[k]);
+    }
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-    const triadContainer = document.getElementById("triad-container")
-
-    function setUpTriadContainer() {
-        applyStyleToNode(
-            triadContainer,
+class ChordContainer extends VisualElement {
+    constructor(node) {
+        super(
+            node,
             {
-                width: `${CONTAINER_SIZE}%`,
-                height: `${CONTAINER_SIZE}%`,
+                width: `${CONTAINER_SIZE}%`, height: `${CONTAINER_SIZE}%`,
                 padding: `${CONTAINER_SIZE * NODE_SIZE / 200}%`,
             },
         );
 
         // Un-hide once properties are set
-        triadContainer.style.display = 'block';
+        this.node.style.display = 'block';
     }
 
-    function cardinalChordBox(name, position) {
-        console.assert(Math.min(...position) >= 0)
-        console.assert(Math.max(...position) <= 1)
-        let node = document.createElement('div');
-        node.textContent = name;
-        node.className = 'cardinal-box';
-        applyStyleToNode(
-            node,
-            {
-                width: `${NODE_SIZE}%`,
-                height: `${NODE_SIZE}%`,
-                left: `${position[1] * 100 - NODE_SIZE / 2}%`,
-                top: `${position[0] * 100 - NODE_SIZE / 2}%`,
-            },
-        );
-        triadContainer.appendChild(node);
+    appendChild(node) {
+        this.node.appendChild(node);
     }
+}
 
-    function triadsBox(text, position) {
-        console.assert(Math.min(...position) >= 0)
-        console.assert(Math.max(...position) <= 1)
-        let node = document.createElement('div');
+class ChordBox extends VisualElement {
+    constructor(container, text, position) {
+        const node = document.createElement('div');
         node.textContent = text;
-        node.className = 'triads-box';
-        applyStyleToNode(
+        node.className = 'chord-box';
+        container.appendChild(node);
+        super(
             node,
             {
-                width: `${NODE_SIZE}%`,
-                height: `${NODE_SIZE}%`,
-                left: `${position[1] * 100 - NODE_SIZE / 2}%`,
-                top: `${position[0] * 100 - NODE_SIZE / 2}%`,
+                width: `${NODE_SIZE}%`, left: `${position[1] * 100 - NODE_SIZE / 2}%`,
+                height: `${NODE_SIZE}%`, top: `${position[0] * 100 - NODE_SIZE / 2}%`,
             },
         );
-        triadContainer.appendChild(node);
+        this.position = position;
+        this.validate();
     }
 
-    cardinalChordBox('C augmented', [0, 0.5]);
-    cardinalChordBox('F augmented', [1, 0]);
-    cardinalChordBox('G augmented', [1, 1]);
+    validate() {
+        console.assert(Math.min(...this.position) >= 0)
+        console.assert(Math.max(...this.position) <= 1)
+    }
+}
 
-    triadsBox('between c and f', [0.5, 0.25])
+document.addEventListener('DOMContentLoaded', function() {
+    const triadContainer = new ChordContainer(
+        document.getElementById("triad-container")
+    )
 
-    setUpTriadContainer();
+    // Cardinal boxes
+    new ChordBox(triadContainer, 'C augmented', [0, 0.5]);
+    new ChordBox(triadContainer, 'F augmented', [1, 0]);
+    new ChordBox(triadContainer, 'G augmented', [1, 1]);
+
+    // Triads boxes
+    new ChordBox(triadContainer, 'between c and f', [0.5, 0.25])
+    new ChordBox(triadContainer, 'between f and g', [1, 0.5])
+    new ChordBox(triadContainer, 'between g and c', [0.5, 0.75])
 })
