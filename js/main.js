4293c36ae2f00e4cbc4fbf457e381af6a23b00c3
rgdl <robert_delisle44@hotmail.com>
2021-06-21 19:03:43 +1000

    begin refactoring main.js

--- js/main.js
+++ js/main.js
@@ -3,141 +3,80 @@ import ReactDOM from 'react-dom';
 
 import { AUG_TRIADS, MAJOR_CHORDS, MINOR_CHORDS } from './chords.js';
 import { ChordContainer } from './visualElements.js';
-import { AsymmetricalChordBox } from './chordBox.js';
 
+import AsymmetricalChordBox from './components/AsymmetricalChordBox.js';
 import CardinalChordBox from './components/CardinalChordBox.js';
-import React_AsymmetricalChordBox from './components/AsymmetricalChordBox.js';
+import { CONTAINER_SIZE, NODE_SIZE } from './consts.js';
 
 
-document.addEventListener('DOMContentLoaded', function() {
-    const triadContainer = new ChordContainer(
-        document.getElementById("triad-container")
-    );
+class App extends React.Component {
+    render() {
+        return (
+			<React.Fragment>
+				<h1>Chromatic Chords</h1>
 
-    // Cardinal boxes
+				<h2>Triads</h2>
+				<div
+					className='triad-container'
+					style={{
+						width: `${CONTAINER_SIZE}%`, height: `${CONTAINER_SIZE}%`,
+						marginTop: `${NODE_SIZE / 2}%`,  marginBottom: `${NODE_SIZE / 2}%`,
+						position: 'relative'
+					}}
+				>
+					<CardinalChordBox theta={3 * Math.PI / 2} chord={AUG_TRIADS['C']} />
+					<CardinalChordBox theta={Math.PI} chord={AUG_TRIADS['F']} />
+					<CardinalChordBox theta={Math.PI / 2} chord={AUG_TRIADS['D']} />
+					<CardinalChordBox theta={0} chord={AUG_TRIADS['G']} />
 
-    let rccb;
+					<AsymmetricalChordBox
+						theta={7 * Math.PI / 4}
+						chordCols={
+							[MAJOR_CHORDS, MINOR_CHORDS].map(chord => ['C', 'E', 'Ab'].map(n => chord[n]))
+						}
+						chordLinks={[
+							[MAJOR_CHORDS['C'], MINOR_CHORDS['C']], [MAJOR_CHORDS['C'], MINOR_CHORDS['E']],
+							[MAJOR_CHORDS['E'], MINOR_CHORDS['E']], [MAJOR_CHORDS['E'], MINOR_CHORDS['Ab']],
+							[MAJOR_CHORDS['Ab'], MINOR_CHORDS['Ab']], [MAJOR_CHORDS['Ab'], MINOR_CHORDS['C']],
+						]}
+					/>
+					<AsymmetricalChordBox
+						theta={1 * Math.PI / 4}
+						chordCols={
+							[MINOR_CHORDS, MAJOR_CHORDS].map(chord => ['G', 'B', 'Eb'].map(n => chord[n]))
+						}
+						chordLinks={[
+							[MAJOR_CHORDS['G'], MINOR_CHORDS['G']], [MAJOR_CHORDS['G'], MINOR_CHORDS['B']],
+							[MAJOR_CHORDS['B'], MINOR_CHORDS['B']], [MAJOR_CHORDS['B'], MINOR_CHORDS['Eb']],
+							[MAJOR_CHORDS['Eb'], MINOR_CHORDS['Eb']], [MAJOR_CHORDS['Eb'], MINOR_CHORDS['G']],
+						]}
+					/>
+					<AsymmetricalChordBox
+						theta={3 * Math.PI / 4}
+						chordCols={
+							[MINOR_CHORDS, MAJOR_CHORDS].map(chord => ['D', 'Gb', 'Bb'].map(n => chord[n]))
+						}
+						chordLinks={[
+							[MAJOR_CHORDS['D'], MINOR_CHORDS['D']], [MAJOR_CHORDS['D'], MINOR_CHORDS['Gb']],
+							[MAJOR_CHORDS['Gb'], MINOR_CHORDS['Gb']], [MAJOR_CHORDS['Gb'], MINOR_CHORDS['Bb']],
+							[MAJOR_CHORDS['Bb'], MINOR_CHORDS['Bb']], [MAJOR_CHORDS['Bb'], MINOR_CHORDS['D']],
+						]}
+					/>
+					<AsymmetricalChordBox
+						theta={5 * Math.PI / 4}
+						chordCols={
+							[MAJOR_CHORDS, MINOR_CHORDS].map(chord => ['F', 'A', 'Db'].map(n => chord[n]))
+						}
+						chordLinks={[
+							[MAJOR_CHORDS['F'], MINOR_CHORDS['F']], [MAJOR_CHORDS['F'], MINOR_CHORDS['A']],
+							[MAJOR_CHORDS['A'], MINOR_CHORDS['A']], [MAJOR_CHORDS['A'], MINOR_CHORDS['Db']],
+							[MAJOR_CHORDS['Db'], MINOR_CHORDS['Db']], [MAJOR_CHORDS['Db'], MINOR_CHORDS['F']],
+						]}
+					/>
+				</div>
+			</React.Fragment>
+        );
+    }
+}
 
-    rccb = document.createElement('div');
-    triadContainer.node.appendChild(rccb);
-    ReactDOM.render(
-        <CardinalChordBox theta={3 * Math.PI / 2} chord={AUG_TRIADS['C']} />,
-        rccb,
-    );
-
-    rccb = document.createElement('div');
-    triadContainer.node.appendChild(rccb);
-    ReactDOM.render(
-        <CardinalChordBox theta={Math.PI} chord={AUG_TRIADS['F']} />,
-        rccb,
-    );
-
-    rccb = document.createElement('div');
-    triadContainer.node.appendChild(rccb);
-    ReactDOM.render(
-        <CardinalChordBox theta={Math.PI / 2} chord={AUG_TRIADS['D']} />,
-        rccb,
-    );
-
-    rccb = document.createElement('div');
-    triadContainer.node.appendChild(rccb);
-    ReactDOM.render(
-        <CardinalChordBox theta={0} chord={AUG_TRIADS['G']} />,
-        rccb,
-    );
-
-    // Triads boxes
-    let racb;
-
-    // TODO: at the App level, invoke service functions to validate the relationships between chords
-    // TODO: how will the chordlinks work?
-
-    racb = document.createElement('div');
-    triadContainer.node.appendChild(racb);
-    ReactDOM.render(
-        <React_AsymmetricalChordBox
-            theta={7 * Math.PI / 4}
-            chordCols={
-                [MAJOR_CHORDS, MINOR_CHORDS].map(chord =>
-                    ['C', 'E', 'Ab'].map(n => chord[n])
-                )
-            }
-            chordLinks={[
-                [MAJOR_CHORDS['C'], MINOR_CHORDS['C']],
-                [MAJOR_CHORDS['C'], MINOR_CHORDS['E']],
-                [MAJOR_CHORDS['E'], MINOR_CHORDS['E']],
-                [MAJOR_CHORDS['E'], MINOR_CHORDS['Ab']],
-                [MAJOR_CHORDS['Ab'], MINOR_CHORDS['Ab']],
-                [MAJOR_CHORDS['Ab'], MINOR_CHORDS['C']],
-            ]}
-        />,
-        racb,
-    );
-
-    racb = document.createElement('div');
-    triadContainer.node.appendChild(racb);
-    ReactDOM.render(
-        <React_AsymmetricalChordBox
-            theta={1 * Math.PI / 4}
-            chordCols={
-                [MINOR_CHORDS, MAJOR_CHORDS].map(chord =>
-                    ['G', 'B', 'Eb'].map(n => chord[n])
-                )
-            }
-            chordLinks={[
-                [MAJOR_CHORDS['G'], MINOR_CHORDS['G']],
-                [MAJOR_CHORDS['G'], MINOR_CHORDS['B']],
-                [MAJOR_CHORDS['B'], MINOR_CHORDS['B']],
-                [MAJOR_CHORDS['B'], MINOR_CHORDS['Eb']],
-                [MAJOR_CHORDS['Eb'], MINOR_CHORDS['Eb']],
-                [MAJOR_CHORDS['Eb'], MINOR_CHORDS['G']],
-            ]}
-        />,
-        racb,
-    );
-
-    racb = document.createElement('div');
-    triadContainer.node.appendChild(racb);
-    ReactDOM.render(
-        <React_AsymmetricalChordBox
-            theta={3 * Math.PI / 4}
-            chordCols={
-                [MINOR_CHORDS, MAJOR_CHORDS].map(chord =>
-                    ['D', 'Gb', 'Bb'].map(n => chord[n])
-                )
-            }
-            chordLinks={[
-                [MAJOR_CHORDS['D'], MINOR_CHORDS['D']],
-                [MAJOR_CHORDS['D'], MINOR_CHORDS['Gb']],
-                [MAJOR_CHORDS['Gb'], MINOR_CHORDS['Gb']],
-                [MAJOR_CHORDS['Gb'], MINOR_CHORDS['Bb']],
-                [MAJOR_CHORDS['Bb'], MINOR_CHORDS['Bb']],
-                [MAJOR_CHORDS['Bb'], MINOR_CHORDS['D']],
-            ]}
-        />,
-        racb,
-    );
-
-    racb = document.createElement('div');
-    triadContainer.node.appendChild(racb);
-    ReactDOM.render(
-        <React_AsymmetricalChordBox
-            theta={5 * Math.PI / 4}
-            chordCols={
-                [MAJOR_CHORDS, MINOR_CHORDS].map(chord =>
-                    ['F', 'A', 'Db'].map(n => chord[n])
-                )
-            }
-            chordLinks={[
-                [MAJOR_CHORDS['F'], MINOR_CHORDS['F']],
-                [MAJOR_CHORDS['F'], MINOR_CHORDS['A']],
-                [MAJOR_CHORDS['A'], MINOR_CHORDS['A']],
-                [MAJOR_CHORDS['A'], MINOR_CHORDS['Db']],
-                [MAJOR_CHORDS['Db'], MINOR_CHORDS['Db']],
-                [MAJOR_CHORDS['Db'], MINOR_CHORDS['F']],
-            ]}
-        />,
-        racb,
-    );
-
-});
+ReactDOM.render(<App />, document.getElementById('root'));
--- js/service.js
+++ js/service.js
@@ -4,8 +4,8 @@ export function sizeAndPositionFromAngle(theta) {
     const results = {
         width: NODE_SIZE + '%',
         height: NODE_SIZE + '%',
-        left: (50 + Math.cos(theta) * 50 - NODE_SIZE / 2) + '%',
-        top: (50 + Math.sin(theta) * 50 - NODE_SIZE / 2) + '%',
+        left: (0 + Math.cos(theta) * 50 - NODE_SIZE / 2) + '%',
+        top: (0 + Math.sin(theta) * 50 - NODE_SIZE / 2) + '%',
     };
     return results;
 }
