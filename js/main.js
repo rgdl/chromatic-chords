c1e8c40849fe7e96cb32829228f8aba25248efbf
rgdl <robert_delisle44@hotmail.com>
2021-08-05 22:13:16 +1000

    update TODOs

--- js/main.js
+++ js/main.js
@@ -9,6 +9,10 @@ import { CONTAINER_SIZE, NODE_SIZE } from './consts.js';
 
 
 // TODO: eventually should have index.js instead of index.html
+// TODO: add regions for selecting scales, to grey out chords
+// TODO: make chord boxes look better
+// TODO: use react lineto to join chord boxes, not the circle thing
+// TODO: use Less CSS
 
 const chordsBetweenCAndG = [MAJOR_CHORDS, MINOR_CHORDS].map(chord => ['C', 'E', 'Ab'].map(n => chord[n]));
 const chordLinksBetweenCAndG = [
