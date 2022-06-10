3668b444b4c13e63f160cc806006d5055d4b2a85
rgdl <robert_delisle44@hotmail.com>
2022-06-10 22:26:11 +1000

    better styling in scale filter box

--- js/components/ScaleFilterBox.js
+++ js/components/ScaleFilterBox.js
@@ -10,12 +10,14 @@ const SCALES = {
     'Major': DIATONIC_SCALES,
 };
 
+const SPACE = "\u00A0";
+
 export default class ScaleFilterBox extends React.Component {
     scaleIsSelected = scale => !!this.props.selectedScales[scale.name]
 
     renderScale = (scale, tonic) => {
         return (
-            <div key={tonic} className="scale-filter-check-box">
+            <div key={tonic}>
                 <input
                     type="checkbox"
                     id={scale.name}
@@ -23,7 +25,9 @@ export default class ScaleFilterBox extends React.Component {
                     checked={this.scaleIsSelected(scale)}
                     onChange={() => this.props.onSelectScale(scale.name)}
                 />
-                <label htmlFor={scale.name}>{tonic}</label>
+                <label htmlFor={scale.name}>
+                    {`${tonic}${tonic.length === 1 ? SPACE : ""}`}
+                </label>
             </div>
         );
     }
@@ -31,14 +35,14 @@ export default class ScaleFilterBox extends React.Component {
     render() {
         return (
             <form className="scale-filter-box">
-                <h2>Filter by scale</h2>
+                <h2>Filter by Scale</h2>
                 {Object.keys(SCALES).map(scaleType => (
-                    <React.Fragment key={scaleType}>
+                    <div key={scaleType}>
                         <h3>{scaleType}</h3>
                         {CIRCLE_OF_FIFTHS_ORDER_NOTE_NAMES.map(tonic => (
                             this.renderScale(SCALES[scaleType][tonic], tonic)
                         ))}
-                    </React.Fragment>
+                    </div>
                 ))}
             </form>
         );
